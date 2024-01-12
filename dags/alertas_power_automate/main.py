87090071ffbfa0d9f359ff5af012955ec42ab49e
ronaldy-prates <ronaldy.santos.terceirizado@sigaantenado.com.br>
2024-01-12 16:44:42 -0300

    removendo função que mostrava as informações do df

--- dags/alertas_power_automate/main.py
+++ dags/alertas_power_automate/main.py
@@ -406,29 +406,6 @@ def new_agendados_semana_atual():
     new_agendados_semana_atual = pd.DataFrame(resultado.fetchall(), columns=resultado.keys())
     return new_agendados_semana_atual
 
-def imprimir_informacao(**kwargs):
-    ti = kwargs['ti']
-    new_agendados_semana_atual = ti.xcom_pull(task_ids='new_agendados_semana_atual')
-    new_agendados_semana_anterior = ti.xcom_pull(task_ids='new_agendados_semana_anterior')
-    iba_semana_anterior = ti.xcom_pull(task_ids='iba_semana_anterior')
-    iba_semana_atual = ti.xcom_pull(task_ids='iba_semana_atual')
-    lista_cidades = ti.xcom_pull(task_ids='lista_cidades')
-    print('new_agendados_semana_atual')
-    print(len(new_agendados_semana_atual))
-    print(new_agendados_semana_atual.head())
-    print('new_agendados_semana_anterior')
-    print(len(new_agendados_semana_anterior))
-    print(new_agendados_semana_anterior.head())
-    print('lista_cidades')
-    print(len(lista_cidades))
-    print(lista_cidades.head())
-    print('iba_semana_anterior')
-    print(len(iba_semana_anterior))
-    print(iba_semana_anterior.head())
-    print('iba_semana_atual')
-    print(len(iba_semana_atual))
-    print(iba_semana_atual.head())
-
 # def envio_banco_dados(**kwargs):
 
 #     ti = kwargs['ti']
@@ -444,8 +421,7 @@ def imprimir_informacao(**kwargs):
 
 default_args = {
     'start_date': datetime(2023, 8, 18, 6, 0, 0),
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5)
+    'retries': None
 }
 
 dag = DAG(
@@ -466,12 +442,6 @@ dag = DAG(
 #     python_callable=extrair_dados_api,
 #     dag=dag
 # ) 
-
-imprimir_informacao = PythonOperator(
-    task_id='imprimir_informacao',
-    python_callable=imprimir_informacao,
-    dag=dag,
-) 
 
 new_agendados_semana_atual = PythonOperator(
     task_id='new_agendados_semana_atual',
