71504da10353440239e29679189852faeca6dd09
ronaldy-prates <ronaldy.santos.terceirizado@sigaantenado.com.br>
2024-01-13 16:41:55 -0300

    modificando valor de timeout para 60 minutos

--- dags/alertas_power_automate/main.py
+++ dags/alertas_power_automate/main.py
@@ -465,14 +465,14 @@ iba_semana_anterior = PythonOperator(
     task_id='iba_semana_anterior',
     python_callable=iba_semana_anterior,
     dag=dag,
-    execution_timeout=timedelta(minutes=30)
+    execution_timeout=timedelta(minutes=60)
 ) 
 
 iba_semana_atual = PythonOperator(
     task_id='iba_semana_atual',
     python_callable=iba_semana_atual,
     dag=dag,
-    execution_timeout=timedelta(minutes=30)
+    execution_timeout=timedelta(minutes=60)
 ) 
 
 # cria_df_final = PythonOperator(
