b07c845be6e2570c11593584ecbbbcdce46132a3
ronaldy-prates <ronaldy.santos.terceirizado@sigaantenado.com.br>
2024-04-14 22:12:36 -0300

    teste para remoção da base de dados

--- dags/onfly/main.py
+++ dags/onfly/main.py
@@ -250,273 +250,40 @@ def politicas(**kwargs):
 
     #deletar colunas 
     df_politica.drop(columns=['data.name_delete','data.createdAt_delete'],axis=1, inplace=True)
-    print(len(df_politica))
-    print(df_politica.head())
-
-def verifica_data_banco():
-    server = Variable.get('DBSERVER')
-    database = Variable.get('DATABASE')
-    username = Variable.get('DBUSER')
-    password = Variable.get('DBPASSWORD')
-    engine = create_engine(f'mssql+pyodbc://{username}:{password}@{server}:1433/{database}?driver=ODBC Driver 18 for SQL Server')
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    consulta_sql = 'SELECT MAX(CAST(dataHoraInicio AS DATE)) data FROM eaf_tvro.ura_datametrica'
-    resultado = session.execute(text(consulta_sql))
-    data_maxima = pd.DataFrame(resultado.fetchall(), columns=resultado.keys())
-
-    data_hoje = datetime.today().date()
-
-    diferenca = (data_hoje - data_maxima.data[0]).days
-    print(f'a diferença entre dias é de: {diferenca}')
-
-    if diferenca >= 2:
-        return 'status_api'
-    return 'nao_faz_nada'
-
-def mensagem_api_fora_do_ar():
-
-    TOKEN = Variable.get("TELEGRAM_DAILY_STATUS_TOKEN")
-    chat_id = Variable.get("TELEGRAM_DAILY_STATUS_ID")
-
-    message = f"EAF-TVRO - URA Datametrica: \n\n Não foi possível atualizar a tabela ura_datametrica, pois a API está for do ar."
-
-    print(message)
-
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-    requests.get(url).json()
-
-def mensagem_sem_info():
-    pass
-
-def mensagem_com_info(**kwargs):
-    pass
-
-def parse_datetime(value):
-    try:
-        if pd.notna(value):
-            cleaned_value = value.replace('T', ' ').split('.')[0]
-            return pd.to_datetime(cleaned_value)
-        else:
-            return pd.NaT
-    except:
-        return pd.NaT
-    
-def status_api():
-    login = json.loads(Variable.get("api_datametrica"))
-
-    try:
-        url_autenticacao = 'https://api-eaf-extrator-genesys.datametrica.com.br/login'
-        response = requests.post(url_autenticacao, json=login)
-        mensagem = 'ok'
-    except Exception as e:
-        mensagem = e
-    if str(mensagem).startswith("HTTPSConnectionPool"):
-        return 'mensagem_api_fora_do_ar'
-    return 'extrair_dados_api'
-
-def extrair_dados_api():
-
-    import os
-    import json
-    import requests
-    import pandas as pd
-    from airflow.models import Variable
-    from datetime import datetime, timedelta
-    from concurrent.futures import ThreadPoolExecutor
-    
-    login = json.loads(Variable.get("api_datametrica"))
-
-    url_autenticacao = 'https://api-eaf-extrator-genesys.datametrica.com.br/login'
-    response = requests.post(url_autenticacao, json=login)
-    jwt_token = response.json()
-
-    inicio_ontem = (datetime.now() - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-    final_ontem = (datetime.now() - timedelta(days=1)).replace(hour=23, minute=59, second=59, microsecond=999999)
-
-    formato = '%Y-%m-%d %H:%M:%S'
-    inicio_ontem = inicio_ontem.strftime(formato)
-    final_ontem = final_ontem.strftime(formato)
-    
-    params = {
-        "dataHoraInicio": inicio_ontem,
-        "dataHoraFim": final_ontem,
-        "pagina": 1
-    }
-
-    headers = {'Authorization': f'Bearer {jwt_token["access_token"]}'}
-    url_recursos_protegidos = 'https://api-eaf-extrator-genesys.datametrica.com.br/chamadas'
-    response_recursos = requests.post(url_recursos_protegidos, headers=headers, json=params)
-    num_paginas = response_recursos.json()['totalPaginas']
-
-    final = pd.DataFrame()
-
-    def fetch_data(page):
-        params['pagina'] = page
-        print(params)
-        response_recursos = requests.post(url_recursos_protegidos, headers=headers, json=params)
-        df = pd.DataFrame(response_recursos.json()['content'])
-        return df
-
-    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-        dfs = executor.map(fetch_data, range(1, num_paginas+1))
-
-    final = pd.concat(list(dfs), ignore_index=True)
-    print('QUANTIDADE DE INFORMAÇÕES:', len(final))
-    
-    return final
-
-def quantidade_registros(**kwargs):
-    ti = kwargs['ti']
-    df_api = ti.xcom_pull(task_ids='extrair_dados_api')
-    print('QUANTIDADE DE INFORMAÇÕES:', len(df_api))
-    if len(df_api) == 0:
-        return 'mensagem_sem_info'
-    return 'tratamento_de_dados'
-
-def tratamento_de_dados(**kwargs):
-
-    def parse_datetime(value):
-        try:
-            if pd.notna(value):
-                cleaned_value = value.replace('T', ' ')
-                return pd.to_datetime(cleaned_value)
-            else:
-                return pd.NaT
-        except:
-            return pd.NaT
-        
-    ti = kwargs['ti']
-    final = ti.xcom_pull(task_ids='extrair_dados_api')
-    print('os dados chegaram aqui:', len(final))
-    print(final.head())
-
-    # convertendo colunas de datas
-    final['dataHoraInicio'] = final['dataHoraInicio'].apply(parse_datetime)
-    final['dataHoraFim'] = final['dataHoraFim'].apply(parse_datetime)
-    final['data_atualizacao'] = date.today().strftime("%d-%m-%Y")
-
-    # tratamento para a coluna atendimento
-    for index, row in final.iterrows():
-        try:
-            tipo = []
-            tempoEspera = []
-            tempoAtendimento = []
-            atendidaAte60Seg = []
-            desistenciaAte60Seg = []
-            atendidaPos60Seg = []
-            desistenciaPos60Seg = []
-
-            for _, i in pd.DataFrame(row['atendimento']).iterrows():
-                tipo.append(i['tipo'])
-                tempoEspera.append(i['tempoEspera'])
-                tempoAtendimento.append(i['tempoAtendimento'])
-                atendidaAte60Seg.append(i['atendidaAte60Seg'])
-                desistenciaAte60Seg.append(i['desistenciaAte60Seg'])
-                atendidaPos60Seg.append(i['atendidaPos60Seg'])
-                desistenciaPos60Seg.append(i['desistenciaPos60Seg'])
-
-            # Atribuir os valores para a linha específica do DataFrame
-            final.at[index, 'tipo'] = str(tipo)
-            final.at[index, 'tempoEspera'] = str(tempoEspera)
-            final.at[index, 'tempoAtendimento'] = str(tempoAtendimento)
-            final.at[index, 'atendidaAte60Seg'] = str(atendidaAte60Seg)
-            final.at[index, 'desistenciaAte60Seg'] = str(desistenciaAte60Seg)
-            final.at[index, 'atendidaPos60Seg'] = str(atendidaPos60Seg)
-            final.at[index, 'desistenciaPos60Seg'] = str(desistenciaPos60Seg)
-
-        except:
-            # Se ocorrer uma exceção, atribuir None para as colunas
-            final.at[index, 'tipo'] = None
-            final.at[index, 'tempoEspera'] = None
-            final.at[index, 'tempoAtendimento'] = None
-            final.at[index, 'atendidaAte60Seg'] = None
-            final.at[index, 'desistenciaAte60Seg'] = None
-            final.at[index, 'atendidaPos60Seg'] = None
-            final.at[index, 'desistenciaPos60Seg'] = None
-
-    # tratamento para a coluna interlocutor
-    for index, row in final.iterrows():
-        try:
-            origem = []
-            cadastroLocalizadoEAF = []
-            uf = []
-            ddd = []
-            codigoCidadeIBGE = []
-            cpf = []
-            cep = []
-            telefone = []
-
-            for _, i in pd.DataFrame([row['interlocutor']]).iterrows():
-                origem.append(i['origem'])
-                cadastroLocalizadoEAF.append(i['cadastroLocalizadoEAF'])
-                uf.append(i['uf'])
-                ddd.append(i['ddd'])
-                codigoCidadeIBGE.append(i['codigoCidadeIBGE'])
-                cpf.append(i['codigoCidadeIBGE'])
-                cep.append(i['cep'])
-                telefone.append(i['telefone'])
-
-            # Atribuir os valores para a linha específica do DataFrame
-            final.at[index, 'origem'] = str(origem)
-            final.at[index, 'cadastroLocalizadoEAF'] = str(cadastroLocalizadoEAF)
-            final.at[index, 'uf'] = str(uf)
-            final.at[index, 'ddd'] = str(ddd)
-            final.at[index, 'codigoCidadeIBGE'] = str(codigoCidadeIBGE)
-            final.at[index, 'cpf'] = str(cpf)
-            final.at[index, 'cep'] = str(cep)
-            final.at[index, 'telefone'] = str(telefone)
-
-        except:
-            # Se ocorrer uma exceção, atribuir None para as colunas
-            final.at[index, 'origem'] = None
-            final.at[index, 'cadastroLocalizadoEAF'] = None
-            final.at[index, 'uf'] = None
-            final.at[index, 'ddd'] = None
-            final.at[index, 'codigoCidadeIBGE'] = None
-            final.at[index, 'cpf'] = None
-            final.at[index, 'cep'] = None
-            final.at[index, 'telefone'] = None
-    final.drop(columns=['atendimento', 'interlocutor', 'csv'], inplace=True)
-
-    def remove_colchetes(valor):
-        if valor == None:
-            return valor
-        return valor.replace('[', '').replace(']', '').replace("'", "")
-
-    # Aplicar a função a todos os elementos do DataFrame
-    final['tipo'] = final['tipo'].apply(remove_colchetes)
-    final['tempoEspera'] = final['tempoEspera'].apply(remove_colchetes)
-    final['tempoAtendimento'] = final['tempoAtendimento'].apply(remove_colchetes)
-    final['atendidaAte60Seg'] = final['atendidaAte60Seg'].apply(remove_colchetes)
-    final['desistenciaAte60Seg'] = final['desistenciaAte60Seg'].apply(remove_colchetes)
-    final['atendidaPos60Seg'] = final['atendidaPos60Seg'].apply(remove_colchetes)
-    final['desistenciaPos60Seg'] = final['desistenciaPos60Seg'].apply(remove_colchetes)
-    final['origem'] = final['origem'].apply(remove_colchetes)
-    final['cadastroLocalizadoEAF'] = final['cadastroLocalizadoEAF'].apply(remove_colchetes)
-    final['uf'] = final['uf'].apply(remove_colchetes)
-    final['ddd'] = final['ddd'].apply(remove_colchetes)
-    final['codigoCidadeIBGE'] = final['codigoCidadeIBGE'].apply(remove_colchetes)
-    final['cpf'] = final['cpf'].apply(remove_colchetes)
-    final['cep'] = final['cep'].apply(remove_colchetes)
-    final['telefone'] = final['telefone'].apply(remove_colchetes)
-    final.rename({'tempoEspera': 'tempoEspera_ms', 'tempoAtendimento': 'tempoAtendimento_ms'}, axis=1, inplace=True)
-    return final
+    return df_politica
 
 def envio_banco_dados(**kwargs):
 
-    ti = kwargs['ti']
-    output = ti.xcom_pull(task_ids='tratamento_de_dados')
-    # output.drop(columns=['id', 'hash', 'csv', 'midia'], axis=1, inplace=True)
+    # ti = kwargs['ti']
+    # colaboradores = ti.xcom_pull(task_ids='colaboradores')
+    # centro_custo = ti.xcom_pull(task_ids='centro_custo')
+    # grupo = ti.xcom_pull(task_ids='grupo')
+    # despesa = ti.xcom_pull(task_ids='despesa')
+    # automovel = ti.xcom_pull(task_ids='automovel')
+    # onibus = ti.xcom_pull(task_ids='onibus')
+    # fatura = ti.xcom_pull(task_ids='fatura')
+    # creditos = ti.xcom_pull(task_ids='creditos')
+    # politicas = ti.xcom_pull(task_ids='politicas')
+
     server = Variable.get('DBSERVER')
-    database = Variable.get('DATABASE')
-    username = Variable.get('DBUSER')
-    password = Variable.get('DBPASSWORD')
+    database = Variable.get('DATABASE-RH')
+    username = Variable.get('DBUSER-RH')
+    password = Variable.get('DBPASSWORD-RH')
     engine = create_engine(f'mssql+pyodbc://{username}:{password}@{server}:1433/{database}?driver=ODBC Driver 18 for SQL Server')
-    # output['data_atualizacao'] = date.today().strftime("%d-%m-%Y")
-    output.to_sql("ura_datametrica", engine, if_exists='append', schema='eaf_tvro', index=False)
+
+    with engine.connect() as connection:
+        print('CRIANDO A CONEXÃO!!!')
+        connection.execute("TRUNCATE TABLE dbo.colaboradores")
+    
+    # colaboradores.to_sql("colaboradores", engine, if_exists='replace', index=False)
+    # centro_custo.to_sql("centro_custo", engine, if_exists='replace', index=False)
+    # grupo.to_sql("grupo", engine, if_exists='replace', index=False)
+    # despesa.to_sql("despesa", engine, if_exists='replace', index=False)
+    # automovel.to_sql("automovel", engine, if_exists='replace', index=False)
+    # onibus.to_sql("onibus", engine, if_exists='replace', index=False)
+    # fatura.to_sql("fatura", engine, if_exists='replace', index=False)
+    # creditos.to_sql("creditos", engine, if_exists='replace', index=False)
+    # politicas.to_sql("politicas", engine, if_exists='replace', index=False)
 
 default_args = {
     'start_date': datetime(2023, 8, 1, 6, 0, 0)
@@ -531,35 +298,35 @@ dag = DAG(
     catchup=False
 )
 
-token_acesso = PythonOperator(
-    task_id='token_acesso',
-    python_callable=token_acesso,
-    dag=dag
-)
+# token_acesso = PythonOperator(
+#     task_id='token_acesso',
+#     python_callable=token_acesso,
+#     dag=dag
+# )
 
-colaboradores = PythonOperator(
-    task_id='colaboradores',
-    python_callable=colaboradores,
-    dag=dag
-)
+# colaboradores = PythonOperator(
+#     task_id='colaboradores',
+#     python_callable=colaboradores,
+#     dag=dag
+# )
 
-centro_custo = PythonOperator(
-    task_id='centro_custo',
-    python_callable=centro_custo,
-    dag=dag
-)
+# centro_custo = PythonOperator(
+#     task_id='centro_custo',
+#     python_callable=centro_custo,
+#     dag=dag
+# )
 
-grupo = PythonOperator(
-    task_id='grupo',
-    python_callable=grupo,
-    dag=dag
-)
+# grupo = PythonOperator(
+#     task_id='grupo',
+#     python_callable=grupo,
+#     dag=dag
+# )
 
-despesa = PythonOperator(
-    task_id='despesa',
-    python_callable=despesa,
-    dag=dag
-)
+# despesa = PythonOperator(
+#     task_id='despesa',
+#     python_callable=despesa,
+#     dag=dag
+# )
 
 # viagens_aereo = PythonOperator(
 #     task_id='viagens_aereo',
@@ -567,34 +334,42 @@ despesa = PythonOperator(
 #     dag=dag
 # )
 
-automovel = PythonOperator(
-    task_id='automovel',
-    python_callable=automovel,
+# automovel = PythonOperator(
+#     task_id='automovel',
+#     python_callable=automovel,
+#     dag=dag
+# )
+
+# onibus = PythonOperator(
+#     task_id='onibus',
+#     python_callable=onibus,
+#     dag=dag
+# )
+
+# fatura = PythonOperator(
+#     task_id='fatura',
+#     python_callable=fatura,
+#     dag=dag
+# )
+
+# creditos = PythonOperator(
+#     task_id='creditos',
+#     python_callable=creditos,
+#     dag=dag
+# )
+
+# politicas = PythonOperator(
+#     task_id='politicas',
+#     python_callable=politicas,
+#     dag=dag
+# )
+
+envio_banco_dados = PythonOperator(
+    task_id='envio_banco_dados',
+    python_callable=envio_banco_dados,
     dag=dag
 )
 
-onibus = PythonOperator(
-    task_id='onibus',
-    python_callable=onibus,
-    dag=dag
-)
+# token_acesso >> colaboradores >> centro_custo >> grupo >> despesa >> automovel >> onibus >> fatura >> creditos >> politicas
 
-fatura = PythonOperator(
-    task_id='fatura',
-    python_callable=fatura,
-    dag=dag
-)
-
-creditos = PythonOperator(
-    task_id='creditos',
-    python_callable=creditos,
-    dag=dag
-)
-
-politicas = PythonOperator(
-    task_id='politicas',
-    python_callable=politicas,
-    dag=dag
-)
-
-token_acesso >> colaboradores >> centro_custo >> grupo >> despesa >> automovel >> onibus >> fatura >> creditos >> politicas
+envio_banco_dados
