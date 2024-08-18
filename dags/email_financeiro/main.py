77fc1ae1ecfaf59b995f360655e724b55b307016
ronaldy-prates <ronaldy.santos.terceirizado@sigaantenado.com.br>
2024-08-17 23:22:52 -0300

    adicionando tratativa

--- dags/email_financeiro/main.py
+++ dags/email_financeiro/main.py
@@ -13,18 +13,20 @@ def extrair_dados_api():
     from airflow.models import Variable
     from sqlalchemy import create_engine
 
-    df = pd.read_csv(StringIO(requests.get('https://api-financial-system-stg.sigaantenado.com.br/financial-system/api/daily-sheet').text))
-    df['data_atualizacao'] = date.today()
-    
-    server = Variable.get('DBSERVER')
-    database = Variable.get('DATABASE')
-    username = Variable.get('DBUSER')
-    password = Variable.get('DBPASSWORD')
-    engine = create_engine(f'mssql+pyodbc://{username}:{password}@{server}:1433/{database}?driver=ODBC Driver 18 for SQL Server')
-    df.to_sql("email_financeiro", engine, if_exists='append', schema='eaf_tvro', index=False)
+    response = requests.get('https://api-financial-system-stg.sigaantenado.com.br/financial-system/api/daily-sheet')
+    if response.text:
+        df = pd.read_csv(StringIO(response.text))
+        df['data_atualizacao'] = date.today()
+        
+        server = Variable.get('DBSERVER')
+        database = Variable.get('DATABASE')
+        username = Variable.get('DBUSER')
+        password = Variable.get('DBPASSWORD')
+        engine = create_engine(f'mssql+pyodbc://{username}:{password}@{server}:1433/{database}?driver=ODBC Driver 18 for SQL Server')
+        df.to_sql("email_financeiro", engine, if_exists='append', schema='eaf_tvro', index=False)
 
 default_args = {
-    'start_date': datetime(2023, 8, 18, 5, 0, 0)
+    'start_date': datetime(2023, 8, 18, 6, 0, 0)
     # 'retries': 3,
     # 'retry_delay': timedelta(minutes=10)
 }
@@ -32,7 +34,7 @@ default_args = {
 dag = DAG(
     'email_financeiro',
     default_args=default_args,
-    schedule_interval='0 5 * * *',
+    schedule_interval='0 12 * * *',
     catchup=False
 )
 
